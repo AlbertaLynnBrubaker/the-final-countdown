334c7419f57ebed9a114a82f54f5397a03aa99a4
Alberta Lynn Brubaker <findingalberta@gmail.com>
2022-06-14 13:59:50 -0500

    JS completely nonfunctional, added content and linked menu git status git status

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-let menuButtons = document.getElementsByClassName("menu-button");
-console.log(menuButtons);
-console.log(typeof menuButtons[1]);
+<script src="./script.js"></script>
+//const menuButtons = document.getElementsByClassName("menu-button");
+//console.log(menuButtons);
+
+
+//console.log(menuButtons);
+
+/*const array = ["menu1", 2, "menu3"];
+
+for(let item of array) {
+    console.log(item);
+}*/
+
 
 function mouseEnter(a, b, c, d) {
     document.getElementById(a).style.backgroundColor = "#20518E";
@@ -14,12 +24,12 @@ function mouseOut(a, b, c, d) {
     document.getElementById(c).src=d;
 }
 
-/*function mouseClick(a, b, c, d) {
+function mouseClick(a, b, c, d) {
     document.getElementById(a).style.backgroundColor = "#FFDEF3";
     document.getElementById(b).style.color = "#20518E";
     document.getElementById(c).src=d;
     if (a === "menu-1") {
-        document.getElementById("content").style.display = "none";
+        document.getElementsByCLassName("content").style.display = "none";
         document.getElementById("content-about-me").style.display = "contents"; 
     };
-}*/
+}
